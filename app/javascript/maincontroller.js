637d223989fa691312bcc05443abadb591a18ae3
Paul Kane <pkane@gannett.com>
2014-05-19 16:53:44 -0400

    Adjusted the ajax call to run within an external for loop. This allowed the iterator to be referenced properly because it wasn't registering a new iterator each time through in the success path.

--- app/javascript/maincontroller.js
+++ app/javascript/maincontroller.js
@@ -4,77 +4,84 @@ app.controller("mainController", function($scope, $http) {
 			scene1 = {
 				id : 'scene1',
 				name : 'news',
+				data : [],
 				src : []
 				// src : 'http://videos.usatoday.net/Brightcove2/29906170001/2014/05/29906170001_3530474700001_usatb5a79027-b22d-43df-84ee-72203c0d6492.mp4'
 			},
 			scene2 = {
 				id : 'scene2',
 				name : 'sports',
+				data : [],
 				src : []
 				// src : 'http://videos.usatoday.net/Brightcove2/29906170001/2014/03/29906170001_3396802596001_Memphis-Regional-Preview.mp4'
 			},
 			scene3 = {
 				id : 'scene3',
 				name : 'money',
+				data : [],
 				src : []
 				// src : 'http://videos.usatoday.net/Brightcove2/29906170001/2014/05/29906170001_3530516356001_usat86ee36cf-a4c8-42ef-ad2b-eb04736edb3e.mp4'
 			},
 			scene4 = {
 				id : 'scene4',
 				name : 'life',
+				data : [],
 				src : []
 				// src : 'http://videos.usatoday.net/Brightcove2/29906170001/2014/05/29906170001_3530582491001_usat6860fe58-4d44-4310-9431-2e90976f53cf.mp4'	
 			},
 			scene5 = {		
 				id : 'scene5',	
 				name : 'tech',
+				data : [],
 				src : []
 				// src : 'http://videos.usatoday.net/Brightcove2/29906170001/2014/05/29906170001_3530293413001_usat2b542485-3ca0-4e86-9bf7-101dd6430608.mp4',
 			},
 			scene6 = {
 				id : 'scene6',
 				name : 'travel',
+				data : [],
 				src : []
 				// src : 'http://videos.usatoday.net/Brightcove2/29906170001/2014/05/29906170001_3529185858001_ProcamsD6141-Strategies-for-Preventing-Skin-Cancer.mp4'
 			}
-		];        
+		];           
         $scope.myData.init = function(item, event) {
         	var key = '1&api_key=zvmv2v9psky7xxhs6sxu825z',
 				url = "http://api.gannett-cdn.com/MobileServices/MArticleService.svc/mcontent/v1/fronts/",
 				fullurl;
 
-			function queryFeed(callback) {
-			    for (var i = $scope.scenes.length - 1; i >= 0; i--) {
-					fullurl = (url + $scope.scenes[i].name + "_Tablet_Video?siteId=" + key);
-					var j = i;
+			function parseData(array, iterator) {
+				for (var i = array[iterator].data.length - 1; i >= 0; i--) {
+					array[iterator].src = array[iterator].data[i].metaData.data.video.FlvUrl;
+					console.log(array[iterator].name, array[iterator].src);			
+				}; 
+			};				
+
+			function queryFeed(i) {
+
+					// $http.get(fullurl).success(function(data) {
+			  		//  $scope.scenes[j].data = data.modules.Items.content;												
+					// 	console.log($http);
+					// 	parseData($scope.scenes, j);
+					//   });
 
 					$.ajax({type: 'GET', url: fullurl, async: true, dataType: 'json', success: function(data){
-								console.log(data);  
-			    				$scope.scenes[j].src = data.modules.Items.content;
+						$scope.scenes[i].data = data.modules.Items.content;																							
+						$scope.$apply(function() {
+							parseData($scope.scenes, i);
+						});  								
+						console.log($scope.scenes)
+			            }, error: function(xhr, ajaxoptions, thrownerror){
+			                alert(xhr.status);
+			                alert(thrownerror);
+			            }
+					});	
 
-					            }, error: function(xhr, ajaxoptions, thrownerror){
-					                alert(xhr.status);
-					                alert(thrownerror);
-					            }});								
-			    };  	
-			    callback();			
+			};
+
+		    for (var i = $scope.scenes.length - 1; i >= 0; i--) {
+				fullurl = (url + $scope.scenes[i].name + "_Tablet_Video?siteId=" + key);
+				queryFeed(i);
 			}
-
-			queryFeed(function () {
-				console.log($scope.scenes);
-			});
-
-
-
-       //      var responsePromise = $http.get(url);
-
-       //      responsePromise.success(function(data, status, headers, config) {
-			    // console.log(data);            	
-       //          $scope.myData.fromServer = data.title;
-       //      });
-       //      responsePromise.error(function(data, status, headers, config) {
-       //          alert("AJAX failed!");
-       //      });		    
 
         };          
 	} );
